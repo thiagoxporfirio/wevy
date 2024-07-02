bdfeff56ab833f515dd6491eb89589b2658863ea
thiagoxporfirio <thiagovporfirio@gmail.com>
2024-07-02 19:12:30 -0300

    router for task

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { createUser } from "./app/controllers/User";
 import { loginUser } from "./app/controllers/loginUser";
 import tokenMiddleware from "./app/Middleware/Authorization";
+import { createTask, deleteTask, getTasks, updateTask } from "./app/controllers/todo";
 
 export const router = Router();
 
@@ -14,3 +15,8 @@ router.get("/", (request, response) => response.json({ message: "." }));
 // ======================
 router.post("/login", loginUser);
 router.post("/register-user", createUser);
+
+router.post("/create-task", tokenMiddleware, createTask);
+router.get("/get-tasks", tokenMiddleware, getTasks);
+router.put("/update-task/:id", tokenMiddleware, updateTask);
+router.delete("/delete-task/:id", tokenMiddleware, deleteTask);
